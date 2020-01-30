bb5f9149ba4e86f93c7e36954677206a8aa0b9be
JANGDORRY <guswns531@gmail.com>
2020-01-30 19:32:53 +0900

    Update maximalRectangle.cpp

--- Week_3/maximalRectangle.cpp
+++ Week_3/maximalRectangle.cpp
@@ -1,3 +1,9 @@
+static const auto _ = []() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
@@ -7,15 +13,15 @@ public:
         
         int max = 0;
 
-        vector<vector<vector<int>>> tmp(x_len, vector<vector<int>>(y_len, vector<int>(3)));
+        vector<vector<vector<int>>> tmp(x_len, vector<vector<int>>(y_len, vector<int>(2)));
 
         for (int i = 0 ; i < x_len; ++i ) {
             for (int j = 0 ; j < y_len ; ++j ) {
                 if(matrix[i][j] == '1') {
                     int x_max = 1 , y_max = 1;
 
-                    if(tmp[i][j][1] == 0 ) tmp[i][j][1]++;
-                    if(tmp[i][j][0] == 0 ) tmp[i][j][0]++;
+                    if(tmp[i][j][1] == 0 ) tmp[i][j][1] = 1;
+                    if(tmp[i][j][0] == 0 ) tmp[i][j][0] = 1;
 
                     if(i + 1 < x_len && matrix[i+1][j] == '1')
                         tmp[i+1][j][1] += tmp[i][j][1] + 1;
@@ -25,48 +31,32 @@ public:
 
 
 
-                    int t_max = 0;
-                    for (int t_y = 1 ; t_y < tmp[i][j][1] ; ++t_y) {
-                        int t_x = tmp[i- t_y][j][0];
-                        if(t_x >= 1 && t_x <= tmp[i][j][0] ) {
-                           if(tmp[i][j- t_x + 1][1] >= (t_y+1)){
-                               t_max = t_max > (t_x) * (t_y+1) ? t_max :  (t_x)  * (t_y+1);
-                           }
-                        } 
+                    int d_x = tmp[i][j][0] - 1;
+                    int d_y = tmp[i][j][1] - 1;
+                    
+                    int t_max = d_x + 1;
+                    //if(max < d_x + 1) max = d_x + 1;
+                    if(t_max < d_y + 1) t_max = d_y + 1;
+                    
+                    //int t_max = d_x > d_y ? d_x + 1 : d_y + 1;
+                    
+                    for (int x = 1 ; x <= d_x; ++x) {
+                        for (int y = 1 ; y <= d_y ; ++y) {
+                            if(tmp[i-y][j][0] - 1 < x)
+                                break;
+                            
+                            int k = (x + 1) * (y + 1);
+                            if(t_max < k) t_max = k;
+                        }
                     }
-
-                    // for (int t_x = 1 ; t_x < tmp[i][j][0] ; ++t_x) {
-                    //     if(tmp[i][j - t_x][1] >= x_max) {
-                    //         y_max++;
-                    //     } else {
-                    //         break;
-                    //     }
-                    // }
-
                     
+                    if(max < t_max) max = t_max;
+                        
                     
-
-                    //tmp[i][j][2] = x_max * y_max;
-                    tmp[i][j][2] = tmp[i][j][0] > tmp[i][j][1] ? tmp[i][j][0] : tmp[i][j][1]; 
-                    //tmp[i][j][2] = tmp[i][j][2] > x_max * y_max ? tmp[i][j][2] :  x_max * y_max;
-                    tmp[i][j][2] = tmp[i][j][2] > t_max ? tmp[i][j][2] :  t_max;
-                    
-                    max = max > tmp[i][j][2] ? max : tmp[i][j][2];
-
-
                 }
             }
         }
 
-        for (int i = 0 ; i < x_len ; ++i ) {
-            for (int j = 0 ; j < y_len ; ++j) 
-                cout << "["<< tmp[i][j][0] << " , " <<  tmp[i][j][1]<<" , "<< tmp[i][j][2] << "] ," ;
-
-            cout << endl;
-        }
-
-        
-        cout << endl;
         return max;
     }
 };
